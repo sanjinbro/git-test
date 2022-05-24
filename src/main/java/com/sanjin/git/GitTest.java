abdd3af2a6764a99bc7980fc4c034f0e65301c0d
sanjin <504804648@qq.com>
2022-05-24 22:53:07 +0800

    first commit

---
+++ src/main/java/com/sanjin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sanjin.git;
+
+/**
+ * @author sanjin
+ * @create 2022-05-24 22:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
